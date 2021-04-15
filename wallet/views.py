898abc52db0d84f0c9cfb3c68e96240dc55398d7
stillnurs <noorsultan.mamataliev@gmail.com>
2021-04-15 16:15:20 +0400

    added models, register view

--- wallet/views.py
+++ wallet/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from bitcoin import *
+import bs4
+import requests
+
+from .models import Details
+
 
 # Create your views here.
 
@@ -9,5 +17,43 @@ def login(request):
     return render(request, 'login.htm')
 
 def register(request):
-    return render(request, 'register.htm')
+    
+    detail = Details()
+    private_key = random_key()
+    public_key = privtopub(private_key)
+    address = pubtoaddr(public_key)
+    detail.private_key = private_key
+    detail.public_key = public_key
+    detail.address = address
+
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        private_key = request.POST['private_key']
+        public_key = request.POST['public_key']
+        address = request.POST['address']
+
+        if password==password2:       
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password, last_name=private_key, first_name=address)
+                user.save();
+                print('User Created')
+                return redirect('login')
+
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('register')
+        return redirect('/')
+
+    else:
+        return render(request, 'register.htm', {'detail': detail})
 
